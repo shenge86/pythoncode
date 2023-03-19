a3e08a118feaa6a9d24e869540f62e7c801bd844
shenge86 <shenge86@gmail.com>
2023-03-18 22:55:11 -0500

    CHINESE：加新单词

--- chinesetest.py
+++ chinesetest.py
@@ -3,6 +3,20 @@ Chinese test
 '''
 import random
 
+words7  = {'Prounciation: ': ('jiāolǜ',''),
+           'Example 1': ('短短几分钟我们就忘记了焦虑与苦恼。','For a few brief minutes we forgot the anxiety and anguish.'),
+           'Example 2': ('当意外事件干扰了你的生活时，你会感到焦虑吗？','Do you feel anxious when unforeseen incidents intrude on your day?'),
+           'Example 3': ('镇静剂有助于缓解焦虑所带来的不安症状。','Tranquillizers help alleviate the distressing symptoms of anxiety.'),
+           'Example 4': ('焦虑是现代人的自然状态。','Anxiety is modern man\'s natural state.'),
+           'Example 5': ('近来公众对这个问题的焦虑心情现在也许正在缓和下来。','The recent public anxiety about this issue may now be abating.')}
+
+words8  = {'Example 1': ('拥挤的人群和喧闹的噪音可能会让游客有些茫然不知所措。','Sightseers may be a little overwhelmed by the crowds and noise.'),
+           'Example 2': ('他边说话边茫然地打量着房间，心不在焉的样子。','He looked vaguely around the room as he spoke, his mind elsewhere.'),
+           'Example 3': ('我茫然地走了35分钟。',' For 35 minutes I was walking around in a daze.'),
+           'Example 4': ('她沉默了一会儿，嘴唇紧闭，眼神茫然。','She was silent for a moment, lips tight shut, eyes distant.'),
+           'Example 5': ('阿博特一脸茫然。“我没听明白，先生。”','Abbot looked blank. \"I don\'t quite follow, sir.\" '),
+           'Example 6': ('他突然改变话题，使她茫然不知所措。','His abrupt change of subject left her floundering helplessly.')}
+
 words14 = {'Pronouniciation': ('Nàmèn',''),
            'Example 1': ('她开始纳闷他在搞什么鬼。','She began to wonder what he was playing at.'),
            'Example 2': ('我时常纳闷她为什么要那样对待我。','I often wonder about why she treated me like that.'),
@@ -20,17 +34,28 @@ words192 = {'Alternative': ('吸脂',''),
 
 words195 = {'Example 1': ('The theory is too difficult to understand, can you elaborate on it?','这个理论太深奥了, 您能不能讲得通透一点?'),
             'Example 2': ('The sky was changing from translucent blue to thicker grey.','天空由通透的湛蓝色变成了暗灰色。'),
-            'Example 3': ('Put each object in front of the lamp . which one is the most transparent', '将物件放在灯座，看看哪一件通透能力最强？')}
+            'Example 3': ('Put each object in front of the lamp . which one is the most transparent', '将物件放在灯座，看看哪一件通透能力最强？')
+            }
 
 words5000= {'Details': ('伦敦西区（London\'s West End）是与纽约百老汇(Broadway)齐名的世界两大戏剧中心之一，是表演艺术的国际舞台，也是英国戏剧界的代名词。','在如此有限的区域内集中如此之多的剧院，在世界上只有纽约的百老汇可与之相比，而从历史传统来讲，西区要比百老汇悠久得多。')}
 
 words = { 1: ('上司','superior'),
           2: ('顾虑','concern; misgivings'),
           3: ('罕见','rare; seldomly seen'),
-         12: ('真挚','sincere (Zhēnzhì)'),
+          4: ('惯用语','idiom'),
+          5: ('奋勇','courageously'),
+          6: ('夹板','splint'),
+          7: ('焦虑','anxiety',words7),
+          8: ('茫然','at a loss',words8),
+          9: ('自白','confession'),
+         10: ('真挚','sincere (zhēnzhì)'),
+         11: ('坠落','fall;drop'),
+         12: ('陈词','statement'),
          13: ('地平线','horizon'),
          14: ('纳闷','wondered (in a perplexed way)',words14),
          15: ('人道主义','humanism'),
+         16: ('膈应','【方言】指讨厌、令人不舒服，但未达到要呕吐的程度。更多的指的是心理上的不舒服。'),
+         17: ('腻歪','【北方方言】1.因次数过多或时间过长而感觉厌烦 2.厌恶 3.无聊 4.一些情侣之间比较亲昵的事，说一些亲昵的话'),
          100: ('接种','vaccinate'),
          101: ('阴性','negative(medical)'),
          102: ('义母','stepmother'),
@@ -65,6 +90,7 @@ words = { 1: ('上司','superior'),
          198: ('吝啬','stingy;miserly'),
          199: ('吝啬鬼','stingy person'),
          200: ('谋生','means of living'),
+         201: ('小龙虾','crawfish'),
          1000: ('旧地重游','Revisit a familiar place; return to old haunts'),
          1001: ('物是人非','The scenery remains the same but the people are changed. Things are unchanged but the people are gone.'),
          1002: ('劳逸结合','Strike a proper balance between work and rest.'),
@@ -86,7 +112,9 @@ words = { 1: ('上司','superior'),
          2000: ('赵大妈','Person who goes around and gets into other people\'s business. Loves to talk to people.'),
          2001: ('一蟹不如一蟹','one crab is no better than the other; worse and worse candidates'),
          2002: ('大姑娘坐花轿——头一回','big girl rides flower carriage--first time; a special first time occurence'),
-         5000: ('伦敦西区','London West End',words5000)}
+         5000: ('伦敦西区','London West End',words5000),
+         5001: ('恐怖小店','Little Shop of Horrors'),
+         }
 
 
 
@@ -109,6 +137,9 @@ if __name__ == '__main__':
     print('你也可以输入quit退出程序。')
     temp = iter(words)
     word_index = next(temp,None)
+
+    nextorrandom = input('Next word or random 顺序下个或者随机? Type in next (下) or random (随): ')
+
     while True:
         chooseword = input(f'Please type the Chinese for: {words[word_index][1]}: ')
         if chooseword in ['give up','gu','next']:
@@ -122,7 +153,6 @@ if __name__ == '__main__':
                     print(v[1])
                 print('===================================================')
 
-            nextorrandom = input('Next word or random 顺序下个或者随机? Type in next (下) or random (随): ')
             if nextorrandom in ['next','n','下','1']:
                 word_index = next(temp,None)
             else:
@@ -137,7 +167,6 @@ if __name__ == '__main__':
             continue
         else:
             print('Correct! Moving to next word. 正确！继续下个词。')
-            nextorrandom = input('Next word or random 顺序下个或者随机? Type in next (下) or random (随): ')
             if nextorrandom in ['next','n','下','1']:
                 word_index = next(temp,None)
             else:
