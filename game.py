ea12c1c4b7e619ff834dcc6e100d5e1fbc185b09
Shen Ge <shenge86@gmail.com>
2025-06-03 22:45:21 -0500

    Create game.py
    Add game.py to repo as the start of something greater

---
+++ game.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  3 22:30:14 2025
+
+@author: sge
+@name: Blood World Final Battle
+@description:
+    
+    The beginnings of a game.
+"""
+
+if __name__ == '__main__':
+    print('Welcome to Blood World Final Battle (血界决战)')
+    #%% Load up or start new game
+
+
+
+    #%% Character Creation / Selection
+    
+    
+    
+    #%% Randomize starting money and city
+    
+    
+    
+    
+    
+    #%% Initialize main game
